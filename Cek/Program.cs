64004768357c8e8d1faefe87df232d63ec4bfb8c
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-11-16 18:52:17 +0700

    Add project files.

---
+++ Cek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
